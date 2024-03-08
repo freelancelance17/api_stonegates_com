ed61e63472d3e04ef8d17934954d05bc33924175
Ubuntu <ubuntu@ip-172-31-30-21.ec2.internal>
2024-03-08 11:14:57 +0000

    first commit

---
+++ api_stonegates_com/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from api_stonegates_com.users.models import User
+from api_stonegates_com.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture()
+def user(db) -> User:
+    return UserFactory()
